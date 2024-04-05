9e6141f2a13d1e311651bd87a25650ca8f6466f9
Javier <javiergondres0@gmail.com>
2024-04-05 17:59:54 -0400

    "build"

--- build/routes/auth/index.js
+++ build/routes/auth/index.js
@@ -3,14 +3,12 @@ import { AuthController } from "../../controllers/auth/index.js";
 import { validateData } from "../../middleware/validateData/index.js";
 import { signInSchema, signUpSchema } from "../../models/auth/schema.js";
 import { ValidateToken } from "../../middleware/verifyJWT/index.js";
-import { roleVerification } from "../../middleware/roleVerification/index.js";
-import { Roles } from "../../types/enum.js";
 export const createAuthRouter = ({ authModel, userSessionCollection, }) => {
     const authRouter = Router();
     const authController = new AuthController(authModel);
     const validateToken = new ValidateToken(userSessionCollection);
     authRouter.post("/signIn", [validateData(signInSchema)], authController.signIn);
     authRouter.post("/signUp", validateData(signUpSchema), authController.signUp);
-    authRouter.post("/signOut", validateToken.validateToken, roleVerification([Roles.USER]), authController.signOut);
+    authRouter.post("/signOut", validateToken.validateToken, authController.signOut);
     return authRouter;
 };
